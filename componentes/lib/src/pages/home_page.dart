6b5bd6e2de388c9b013ef9c7aa1ddfb30b9e454d
Juan Carlos Perez Zapata <35706696+jcperezz@users.noreply.github.com>
2021-07-21 16:02:01 -0500

    63. Pagina Home

---
+++ componentes/lib/src/pages/home_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Componentes'),
+      ),
+      body: _lista(),
+    );
+  }
+
+  Widget _lista() {
+    return ListView(
+      children: _listaItems(),
+    );
+  }
+
+  List<Widget> _listaItems() {
+    return [];
+  }
+}
