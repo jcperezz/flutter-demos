282804851a481239f4832a85281f24fac593a3e9
Juan Carlos Perez Zapata <35706696+jcperezz@users.noreply.github.com>
2021-07-22 16:45:43 -0500

    68. Navegacion basica

--- componentes/lib/src/pages/home_page.dart
+++ componentes/lib/src/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:componentes/src/pages/alert_page.dart';
 import 'package:componentes/src/providers/menu_provider.dart';
 import 'package:componentes/src/utils/icon_string_util.dart';
 import 'package:flutter/material.dart';
@@ -21,13 +22,13 @@ class HomePage extends StatelessWidget {
       initialData: [],
       builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
         return ListView(
-          children: _listaItems(snapshot.data),
+          children: _listaItems(snapshot.data, context),
         );
       },
     );
   }
 
-  List<Widget> _listaItems(List<dynamic>? data) {
+  List<Widget> _listaItems(List<dynamic>? data, BuildContext context) {
     print(data);
 
     List<Widget> items = [];
@@ -41,7 +42,10 @@ class HomePage extends StatelessWidget {
             Icons.keyboard_arrow_right,
             color: Colors.blue,
           ),
-          onTap: () {},
+          onTap: () {
+            final route = MaterialPageRoute(builder: (context) => AlertPage());
+            Navigator.push(context, route);
+          },
         ))
         ..add(Divider());
     });
