e5808b61bd6ee01282eb2be566d0f54286f723a5
Juan Carlos Perez Zapata <35706696+jcperezz@users.noreply.github.com>
2021-07-23 10:01:31 -0500

    77. Widget circle avatar

--- componentes/lib/src/pages/avatar_page.dart
+++ componentes/lib/src/pages/avatar_page.dart
@@ -8,6 +8,29 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar Page'),
+        actions: <Widget>[
+          Container(
+            padding: EdgeInsets.all(5.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://www.w3schools.com/w3images/avatar2.png'),
+              radius: 25.0,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text('JC'),
+              backgroundColor: Colors.brown,
+            ),
+          )
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+          image: NetworkImage('https://www.w3schools.com/w3images/avatar2.png'),
+          placeholder: AssetImage('assets/jar-loading.gif'),
+        ),
       ),
     );
   }
