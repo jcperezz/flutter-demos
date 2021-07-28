68d47b43a7a7351a63952b4b4ba8131398042c95
Juan Carlos Perez Zapata <35706696+jcperezz@users.noreply.github.com>
2021-07-28 15:05:24 -0500

    98. MovieSlider Horizontal

--- peliculas/lib/widgets/card_swiper.dart
+++ peliculas/lib/widgets/card_swiper.dart
@@ -1,14 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_card_swipper/flutter_card_swiper.dart';
 
 class CardSwiper extends StatelessWidget {
   const CardSwiper({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final _size = MediaQuery.of(context).size;
+
     return Container(
       width: double.infinity,
-      height: 500,
-      color: Colors.red,
+      height: _size.height * 0.5,
+      child: Swiper(
+        itemCount: 10,
+        layout: SwiperLayout.STACK,
+        itemWidth: _size.width * 0.6,
+        itemHeight: _size.height * 0.4,
+        itemBuilder: (_, int index) {
+          return GestureDetector(
+            onTap: () => Navigator.pushNamed(context, 'details',
+                arguments: 'movie-instance'),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20), // Redondea los bordes
+              child: FadeInImage(
+                placeholder: AssetImage(
+                    'assets/no-image.jpg'), // Se debe registrar el recurso en el pubspec.yaml
+                image: NetworkImage('https://via.placeholder.com/300x400'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 }
