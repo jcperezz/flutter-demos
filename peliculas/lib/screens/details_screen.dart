33469d8aa34454dcaf8b01b1f03eb608a5a8d368
Juan Carlos Perez Zapata <35706696+jcperezz@users.noreply.github.com>
2021-07-29 15:19:21 -0500

    100. Movie Details Slivers

--- peliculas/lib/screens/details_screen.dart
+++ peliculas/lib/screens/details_screen.dart
@@ -5,10 +5,42 @@ class DetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final String movie =
+        ModalRoute.of(context)?.settings.arguments.toString() ?? 'no-movie';
+
     return Scaffold(
-      body: Container(
-        child: Center(
-          child: Text('Details Screen'),
+      body: CustomScrollView(
+        slivers: [
+          _CustomAppBar(),
+        ],
+      ),
+    );
+  }
+}
+
+class _CustomAppBar extends StatelessWidget {
+  const _CustomAppBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      backgroundColor: Colors.indigo,
+      expandedHeight: 200,
+      floating: false,
+      pinned: true,
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        titlePadding: EdgeInsets.all(0),
+        title: Container(
+          width: double.infinity,
+          alignment: Alignment.bottomCenter,
+          color: Colors.black12,
+          child: Text('movie.title'),
+        ),
+        background: FadeInImage(
+          image: NetworkImage('https://via.placeholder.com/500x300'),
+          placeholder: AssetImage('assets/loading.gif'),
+          fit: BoxFit.cover,
         ),
       ),
     );
