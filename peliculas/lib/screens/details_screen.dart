04792dbbabc7aa2011dfbc153c7d9f79c6a849f7
Juan Carlos Perez Zapata <35706696+jcperezz@users.noreply.github.com>
2021-08-02 16:02:23 -0500

    113. Casting

--- peliculas/lib/screens/details_screen.dart
+++ peliculas/lib/screens/details_screen.dart
@@ -19,7 +19,7 @@ class DetailsScreen extends StatelessWidget {
               delegate: SliverChildListDelegate([
             _PosterAndTitle(movie: movie),
             _Overview(movie: movie),
-            CastingCards(),
+            CastingCards(movie.id),
           ])),
         ],
       ),
